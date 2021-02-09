af318b3431f4aeace8c889e26aa656ffc0388965
Alessandro De Marco <lookatde.lad@gmail.com>
2021-02-09 14:12:03 +0100

    BONUS

--- js/main.js
+++ js/main.js
@@ -2,16 +2,35 @@ var app = new Vue({
   el: '#app',
   data: {
     dischi: [],
-    generi: ['All']
+    generi: ['All'],
+    selected: 'All',
+    selectedDischi: []
   },
   mounted() {
     axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((result) => {
       this.dischi = result.data.response;
+      this.selectedDischi = result.data.response;
       this.dischi.forEach((element, i) => {
         if (!this.generi.includes(element.genre)) {
           this.generi.push(element.genre)
         }
       });
     }).catch(error => alert('Errore!'))
+  },
+  methods: {
+    changeItem(event) {
+      this.selected = event.target.value;
+      this.selectedDischi = [];
+      this.dischi.forEach((element, i) => {
+        if (this.selected == 'All') {
+          this.selectedDischi = this.dischi;
+        }
+        else {
+          if (element.genre.includes(this.selected)) {
+            this.selectedDischi.push(element);
+          }
+        }
+      });
+    }
   }
 });
